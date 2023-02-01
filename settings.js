fbf0828763258a7f9af8069d2e7cafd2b09702f1
sandaruboy <117792676+sandaruboy@users.noreply.github.com>
2023-02-01 12:08:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,14 +12,14 @@ global.headerText = "Anya by Pika" // text for header of menu
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
+global.ownername = "*𝘴𝘢𝘯𝘥𝘢𝘳𝘶 𝘨𝘪𝘮𝘴𝘢𝘳𝘢*"   // Owner name //        
+global.ownernumber = ['94783662147'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
+global.region = "Sri Lanka" // your country name
 global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.district = "Kuruneegala" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
